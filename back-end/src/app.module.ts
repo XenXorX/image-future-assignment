8848302a7a98391d103bb7c9bbc936983d020989
XenXorX <vampiricscepter@gmail.com>
2025-06-08 17:43:11 +0700

    Add NewsService

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsController } from './news/news.controller';
+import { NewsService } from './news/news.service';
 
 @Module({
   imports: [],
   controllers: [AppController, NewsController],
-  providers: [AppService],
+  providers: [AppService, NewsService],
 })
 export class AppModule {}
---
+++ back-end/src/news/news.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class NewsService {
+    getNews() {
+
+    }
+
+    crateNews() {
+
+    }
+
+    updateNews() {
+
+    }
+}
