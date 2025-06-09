3fdfadab9db4b8068581adeda54d48383625a9cb
XenXorX <vampiricscepter@gmail.com>
2025-06-09 18:37:23 +0700

    Update createNews and refactor getNews

--- back-end/src/news/news.controller.ts
+++ back-end/src/news/news.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpStatus, Post, Put, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Put, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { Observer } from 'rxjs';
 import { AxiosResponse } from 'axios';
@@ -12,8 +12,8 @@ export class NewsController {
     @Get()
     getNews(@Res() res: Response) {
         const observer: Observer<AxiosResponse<News[]>> = {
-            next: news => {
-                res.status(HttpStatus.OK).json(news.data);
+            next: value => {
+                res.status(HttpStatus.OK).json(value.data);
             },
             error: err => {
                 res.sendStatus(HttpStatus.NOT_FOUND);
@@ -21,12 +21,22 @@ export class NewsController {
             complete: () => { }
         };
 
-        this.newsService.getNews().subscribe(observer);
+        this.newsService.findAll().subscribe(observer);
     }
 
     @Post()
-    crateNews() {
+    createNews(@Body() news: News, @Res() res: Response) {
+        const observer: Observer<AxiosResponse> = {
+            next: value => {
+                res.sendStatus(HttpStatus.NO_CONTENT);
+            },
+            error: err => {
+                res.sendStatus(HttpStatus.BAD_REQUEST);
+            },
+            complete: () => { }
+        };
 
+        this.newsService.create(news).subscribe(observer);
     }
 
     @Put()
--- back-end/src/news/news.service.ts
+++ back-end/src/news/news.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { Observable } from 'rxjs';
-import { AxiosResponse } from 'axios';
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { News } from './news.interface';
 
 @Injectable()
 export class NewsService {
-    private readonly url = 'http://localhost:3001';
+    private readonly url = 'http://localhost:3001/news';
 
     constructor(private readonly httpService: HttpService) { }
 
-    getNews(): Observable<AxiosResponse<News[]>> {
-        return this.httpService.get(`${this.url}/news?_sort=-update_date&_limit=17`);
+    findAll(): Observable<AxiosResponse<News[]>> {
+        return this.httpService.get(`${this.url}?_sort=-update_date&_limit=17`);
     }
 
-    crateNews() {
+    create(body: News): Observable<AxiosResponse> {
+        const config: AxiosRequestConfig = {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        };
 
+        return this.httpService.post(this.url, body, config);
     }
 
     updateNews() {
