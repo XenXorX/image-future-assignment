f4adedf6a27cb45b200cec6778cc504ee5d850f4
XenXorX <vampiricscepter@gmail.com>
2025-06-10 09:57:35 +0700

    Update sideNews layout

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,3 +1,8 @@
+function convertDate(dateString: string): string {
+  const date = new Date(dateString);
+  return date.toLocaleDateString();
+}
+
 export default async function Home() {
   const newsData = await fetch(`http://localhost:3000/news`)
   const newsArray = await newsData.json()
@@ -7,13 +12,24 @@ export default async function Home() {
 
   return (
     <main>
-      <div className="flex flex-row">
-        <div className="divider divider-neutral" />
+      <div className="grid grid-cols-3 gap-4">
 
-        <div className="basis-2/3">
+        <div className="col-span-2">
           <div className="carousel w-full">
             {highlightNews.map((news: any) => (
               <div id={'news_' + news.entry_id} className="carousel-item w-full">
+                <div className="card card-side bg-base-100 shadow-sm">
+                  <figure>
+                    <img
+                      src={news.img_src}
+                      alt={news.title} />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title">{news.title}</h2>
+                    <span>{convertDate(news.update_date)}</span>
+                    <span className="badge badge-neutral badge-outline justify-end">{news.channel_name}</span>
+                  </div>
+                </div>
                 <img
                   src={news.img_src}
                   className="w-full" />
@@ -27,12 +43,28 @@ export default async function Home() {
           </div>
         </div>
 
-        <div className="basis-1/3">
-
+        <div className="col-span-1">
+          {sideNews.map((news: any) => (
+            <div className="flex justify-center">
+              <div className="card bg-base-100 w-96 m-2 shadow-sm">
+                <figure>
+                  <img
+                    src={news.img_src}
+                    alt={news.title} />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{news.title}</h2>
+                  <div className="card-actions justify-end">
+                    <span>{convertDate(news.update_date)}</span>
+                    <div className="badge badge-neutral badge-outline">{news.channel_name}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
 
-        <div className="divider divider-neutral" />
       </div>
-    </main>
+    </main >
   );
 }
